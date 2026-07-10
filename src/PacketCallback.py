ea33d772f36c9f902ba732fba57f5c012a3d70a3
= <rossgardiner24@gmail.com>
2026-07-10 13:23:17 +0100

    add base classes for the pipeline

---
+++ src/PacketCallback.py
@@ -0,0 +1,14 @@
+# PacketCallback
+# This is a simple class which defines a callback in my chain
+# Classes which inherit PacketCallback must implement the next_packet method.
+
+from abc import ABC, abstractmethod
+
+
+class PacketCallback(ABC):
+    @abstractmethod
+    def next_packet(self, packet: AudioPacket) -> None:
+        """
+        Receive a new AudioPacket.
+        """
+        raise NotImplementedError
---
+++ src/PipelineLink.py
@@ -0,0 +1,22 @@
+# PipelineLink 
+# This class simply extends PacketCallback to allow the registration of a further callback in the chain. 
+# A PipelineLink instance can therefore both recieve and transmit AudioPackets
+
+from src.PacketCallback import PacketCallback
+from src.AudioPacket import AudioPacket
+
+class PinelineLink(PacketCallback):
+	def __init__(self):
+		self.callback = None
+		
+	def register_callback(self, callback: AudioPacket)
+		self.callback = callback
+	
+	def next_packet(self, packet: AudioPacket):
+		if self.callback is None:
+			return 
+		else:
+			self.callback(packet)
+		
+
+		
