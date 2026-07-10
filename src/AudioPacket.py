fe7fae7106f2e71d87c4628cfdfdb225672460eb
= <rossgardiner24@gmail.com>
2026-07-10 12:42:34 +0100

    adds AudioPacket, progresses #6

---
+++ src/AudioPacket.py
@@ -0,0 +1,41 @@
+# AudioPacket 
+# Dataclass which is treated as the basic callback unit for this application
+# This stores the file(s) location(s) attributed to a given audio event, it also stores the relevant metadata to carry forward through the programme. 
+# AudioPacket is general purpose, so it is designed to carry CARACAL data and data from other audio sources, I've tried to select the right level of abstraction for this. 
+# The AudioPackets will be modifed through the pipeline. Critically, the structure keeps track whether it has been processed with an AI model yet, see embeddings_point_dict and is_whole
+# A given AudioPacket could represent a long sequence of so far unprocessed audio, or, a much shorter snippet of interest (e.g. a "detection" from an AI model). 
+# AudioPacket does not store raw data, it points to everything on disk. 
+
+from dataclasses import dataclass, field
+from uuid import uuid4
+
+@dataclass
+class AudioPacket:
+    # UUID field to keep track of things
+    id: str = field(default_factory=lambda: uuid4().hex)
+
+    # Ordered list of continuous audio files
+    audio_paths: list[str] = field(default_factory=list)
+    
+    # Offset, in seconds, to the part of the audio of interest
+    offset: float = 0.0
+    
+    # Duration, in seconds, of the audio of interest
+    duration: float | None = None 
+    
+    # Is the snippet of interest the entire collection of audio?
+    is_whole: bool = True
+    
+    # Latitude, Longitude, for spatial processing
+    lat : float | None = None
+    lon : float | None  = None
+    
+    # Unix time of the capture start 
+    capture_start_time: int | None = None
+    
+    # Misc metadata, a rough place to store other metadata associated with this AudioPacket
+    misc_metadata : dict = field(default_factory=dict)
+    
+    # Dictionary to store pointers on disk to embeddings attributed to this AudioPacket
+    embeddings_point_dict : dict = field(default_factory=dict)
+	
