9373cf2d21dc1d0044c54a549bd37038dde9342f
= <rossgardiner24@gmail.com>
2026-08-14 15:48:50 +0100

    audio buffer id field is a product of its identifying properties

--- src/AudioBuffer.py
+++ src/AudioBuffer.py
@@ -7,11 +7,15 @@
 import numpy as np
 from dataclasses import dataclass, field
 from uuid import uuid4
+from hashlib import sha256
 
 from src.AudioPacket import AudioPacket
 
 
-
+def _make_id_of_audio_buffer(packet, sample_rate, start_offset_s, valid_samples):
+    identity_string = f"{packet.id} + {sample_rate}, + {start_offset_s} + {valid_samples}"
+    return sha256(identity_string.encode("utf-8")).hexdigest()
+    
 @dataclass
 class AudioBuffer:
 
@@ -34,5 +38,17 @@ class AudioBuffer:
     
     embeddings = {}
     
-    # UUID field to keep track of things
-    id: str = field(default_factory=lambda: uuid4().hex)
+    # a buffers id is a hash of its idenfiying properties
+    id: str = field(
+        init=False
+    )
+
+    def __post_init__(self):
+        self.id = _make_id_of_audio_buffer(
+            self.packet,
+            self.sample_rate,
+            self.start_offset_s,
+            self.valid_samples,
+        )
+    
+
